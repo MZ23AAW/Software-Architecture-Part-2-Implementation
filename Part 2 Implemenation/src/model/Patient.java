771e4920e4e7f3bed57d3c2bb7798d24d0e658ce
Wahid-31 <mz23aaw@herts.ac.uk>
2025-12-31 15:07:13 +0000

    Update Patient.java
    Coding

--- Part 2 Implemenation/src/model/Patient.java
+++ Part 2 Implemenation/src/model/Patient.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Patient {
+    private String patientId;
+    private String firstName;
+    private String lastName;
+    private String nhsNumber;
+
+    public Patient(String patientId, String firstName, String lastName, String nhsNumber) {
+        this.patientId = patientId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nhsNumber = nhsNumber;
+    }
+    public String getPatientId() { return patientId}
+    public String getFirstName() { return firstName;}
+    public String getLastName() { return lastName;}
+    public String getNhsNumber() { return nhsNumber; }
 }
+
